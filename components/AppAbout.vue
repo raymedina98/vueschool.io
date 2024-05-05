f7582f27d72d2eb8552645818fbd29c87328479a
raymedina98 <raymedina.dev@gmail.com>
2024-05-04 21:12:30 -0400

    fix: fix about section spaces

--- components/AppAbout.vue
+++ components/AppAbout.vue
@@ -3,10 +3,5 @@
     <section class="section-container">
       <AboutCard />
     </section>
-    <section
-      class="section-container mt-14 hidden flex-row items-center justify-around lg:flex"
-    >
-      <Icon v-for="i in 6" :key="i" name="GoogleIcon" class="h-10 w-[119px]" />
-    </section>
   </div>
 </template>
